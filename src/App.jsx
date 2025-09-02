686d2833f646c93dbe2bf64bba48f7c602e9fc7a
bqprojects <support@brainquest.info>
2025-09-02 18:23:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Aran from "./Aran";
+
+function App() {
+  return (
+    <>
+      <div className="text-red-400">
+        <Aran />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Aran.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Aran = () => {
+  return <div></div>;
+};
+
+export default Aran;
